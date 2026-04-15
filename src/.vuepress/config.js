6a60488cb11bc69a63025cfa402252b47ebbd3d1
nfraz007 <nfraz007@gmail.com>
2026-04-15 14:33:51 +0530

    fixed url

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,17 +1,15 @@
 module.exports = {
-    title: "Nazish Fraz",
-    description: "Docs repository",
-    themeConfig: {
-        nav: [
-            { text: 'Portfolio', link: 'https://me.nfraz.co.in' },
-        ],
-        sidebar: 'auto'
-    },
-    plugins: [
-        '@vuepress/last-updated', 
-        '@vuepress/back-to-top', 
-        '@vuepress/medium-zoom',
-        'vuepress-plugin-smooth-scroll',
-        'vuepress-plugin-nprogress'
-    ]
-}
+  title: "Nazish Fraz",
+  description: "Docs repository",
+  themeConfig: {
+    nav: [{ text: "Portfolio", link: "https://nfraz.co.in" }],
+    sidebar: "auto",
+  },
+  plugins: [
+    "@vuepress/last-updated",
+    "@vuepress/back-to-top",
+    "@vuepress/medium-zoom",
+    "vuepress-plugin-smooth-scroll",
+    "vuepress-plugin-nprogress",
+  ],
+};
